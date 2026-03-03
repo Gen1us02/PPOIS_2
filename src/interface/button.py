dde9be8b4c879d17f1318b62d24f8625472ff33e
Zhenya <yevgeniy_kulik@list.ru>
2026-03-03 12:54:16 +0300

    [refactor] button class

---
+++ src/interface/button.py
@@ -0,0 +1,41 @@
+from typing import Optional, Tuple
+from src.states import States
+from pygame import Surface
+import pygame
+
+
+class Button:
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        text: str,
+        color: Tuple[int, int, int],
+        hover_color: Tuple[int, int, int],
+        action: States,
+    ) -> None:
+        self.__rect = pygame.Rect(x, y, width, height)
+        self.__text = text
+        self.__color = color
+        self.__hover_color = hover_color
+        self.__action = action
+        self.__font = pygame.font.Font("assets/fonts/Sjz.otf", 30)
+
+    def draw(self, screen: Surface) -> None:
+        mouse_pos = pygame.mouse.get_pos()
+        current_color = (
+            self.__hover_color if self.__rect.collidepoint(mouse_pos) else self.__color
+        )
+
+        pygame.draw.rect(screen, current_color, self.__rect)
+        text = self.__font.render(self.__text, True, (0, 0, 0))
+        text_rect = text.get_rect(center=self.__rect.center)
+        screen.blit(text, text_rect)
+
+    def check_event(self, event) -> Optional[States]:
+        if self.__rect.collidepoint(event.pos):
+            return self.__action
+        
+        return None
---
+++ src/interface/menu.py
@@ -0,0 +1,95 @@
+from typing import Optional
+from pygame import Surface
+from src.states import States
+from src.interface.button import Button
+from src.config import config
+import pygame
+
+
+BUTTON_WIDTH = int(config["DEFAULT"]["BUTTON_WIDTH"])
+BUTTON_HEIGHT = int(config["DEFAULT"]["BUTTON_HEIGHT"])
+BUTTON_Y = int(config["DEFAULT"]["BUTTON_Y"])
+BUTTON_PADDING = int(config["DEFAULT"]["BUTTON_PADDING"])
+
+
+class Menu:
+    def __init__(self, screen: Surface) -> None:
+        self.screen = screen
+        button_x = (self.screen.get_width() - BUTTON_WIDTH) // 2
+        self.buttons = [
+            Button(
+                button_x,
+                BUTTON_Y,
+                BUTTON_WIDTH,
+                BUTTON_HEIGHT,
+                "Начать",
+                (186, 2, 2),
+                (230, 57, 57),
+                States.START,
+            ),
+            Button(
+                button_x,
+                BUTTON_Y + BUTTON_PADDING,
+                BUTTON_WIDTH,
+                BUTTON_HEIGHT,
+                "Таблица лидеров",
+                (186, 2, 2),
+                (230, 57, 57),
+                States.LEADERS,
+            ),
+            Button(
+                button_x,
+                BUTTON_Y + BUTTON_PADDING * 2,
+                BUTTON_WIDTH,
+                BUTTON_HEIGHT,
+                "Справка",
+                (186, 2, 2),
+                (230, 57, 57),
+                States.RULES,
+            ),
+            Button(
+                button_x,
+                BUTTON_Y + BUTTON_PADDING * 3,
+                BUTTON_WIDTH,
+                BUTTON_HEIGHT,
+                "Выход",
+                (186, 2, 2),
+                (230, 57, 57),
+                States.EXIT,
+            ),
+        ]
+        self.bg = pygame.image.load("assets/images/menu_background.png").convert_alpha()
+        self.bg = pygame.transform.scale(
+            self.bg, (self.screen.get_width(), self.screen.get_height())
+        )
+        self.game_name = pygame.image.load("assets/images/name.png").convert_alpha()
+        self.game_name = self._scale_image(self.game_name, target_width=400)
+
+        self.name_rect = self.game_name.get_rect()
+        self.name_rect.centerx = self.screen.get_width() // 2
+        self.name_rect.y = -100
+
+    def handle_events(self) -> Optional[str]:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return States.EXIT
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for button in self.buttons:
+                    action = button.check_event(event)
+                    if action:
+                        return action
+
+        return None
+
+    def _scale_image(self, image: Surface, target_width: int):
+        original_width, original_height = image.get_size()
+        scale_factor = target_width / original_width
+        new_height = int(original_height * scale_factor)
+        return pygame.transform.smoothscale(image, (target_width, new_height))
+
+    def draw(self) -> None:
+        self.screen.blit(self.bg, (0, 0))
+        self.screen.blit(self.game_name, self.name_rect)
+        for button in self.buttons:
+            button.draw(self.screen)
